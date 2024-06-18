784925485601ff64e09aa638990ff117121ad398
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2024-06-18 07:10:37 -0400

    Backend/Add and get request API created and tested
    In testing use thunder client

--- API/server.js
+++ API/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import { Books } from "./BookModel.js";
+import bodyParsher from "express";
 
 const app = express();
-
+app.use(bodyParsher.json());
 //connect mongoodb database
 mongoose
   .connect(
@@ -14,9 +15,32 @@ mongoose
   .catch((err) => console.log(err));
 
 //get all books
+app.get("/", async (req, res) => {
+  try {
+    let books = await Books.find();
+    res.json(console.log({ message: "Books found", books }));
+  } catch (err) {
+    res.json(console.log("book not foud"));
+  }
+});
 
 //add book
-
+app.post("/", async (req, res) => {
+  const { Book_name, Book_publisher, Book_type, Book_description } = req.body;
+  try {
+    let get_book = await Books.findOne({ Book_name });
+    if (get_book) return res.json({ message: "Book alredy exist" });
+    let book = await Books.create({
+      Book_name,
+      Book_publisher,
+      Book_type,
+      Book_description,
+    });
+    res.json({ message: "Book added successfully", book });
+  } catch (error) {
+    res.json({ message: error.message() });
+  }
+});
 //delete book
 
 //update book
