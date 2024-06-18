d8327cc086b5cf816347675e59d0bd2e95a7ea1d
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2024-06-17 21:29:45 -0400

    Backend/connect MongoDB with Express

--- API/server.js
+++ API/server.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
-app.get("/", (req, res) => {
-  res.json({ message: "Server is runing on 2000" });
-});
+
+//connect mongoodb database
+mongoose
+  .connect(
+    "mongodb+srv://mavarnimisha95764:Y5QKssgpfDPTUAgE@cluster0.hkyow3s.mongodb.net/",
+    { dbName: "MERN_Books_Management_Syatem" }
+  )
+  .then(() => console.log("MongoDB connected succesfully"))
+  .catch((error) => console.log(error));
+
+// app.get("/", (req, res) => {
+//   res.json({ message: "Server is runing on 2000" });
+// });
 app.listen(2000, () => console.log("Server is runing on 2000"));
+
+//mavarnimisha95764 - username
+//Y5QKssgpfDPTUAgE - password
