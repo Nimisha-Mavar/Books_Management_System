a3a7d0a1f939469d5939490d82b22e1e2372f2ee
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2024-06-18 21:12:11 -0400

    Frontend/update Book component

--- Client/src/Book.jsx
+++ Client/src/Book.jsx
@@ -13,6 +13,7 @@ const Book = ({ books }) => {
               <div className="card-body">
                 <h5 className="card-title">{book.Book_name}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">
+                  <i class="fa-solid fa-user-tie"></i>&nbsp;&nbsp;
                   {book.Book_publisher}
                 </h6>
                 <p className="card-subtitle mb-2 text-muted">
